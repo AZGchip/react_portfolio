fc65c8413bb101e706e5c6d76c7b713b6584177f
AZGchip <60306563+AZGchip@users.noreply.github.com>
2020-06-30 15:45:14 -0400

    added images forder to application. updated image paths

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,11 +1,11 @@
 import React from "react";
-
+import List from "./List"
 
 
 function Container(props) {
 
     return <div>
-        
+        <List/>
     </div>;
 }
 export default Container
--- src/components/List.js
+++ src/components/List.js
@@ -2,9 +2,9 @@ import React from "react"
 const listData = [
     {
         id: 1,
-        image: "assets/images/New Bitmap Image.bmp",
+        image: "../assets/images/trales_ales.bmp",
         alt: "Ales and Trails",
-        title:"Trails & Ales (Collab) ",
+        title: "Trails & Ales (Collab) ",
         summary: "A site utilizing web APIs to display the location of trails and breweries around a searched location on an interactive map.",
         description: `<p>A collaboration with:
         <ul>
@@ -19,9 +19,9 @@ const listData = [
     },
     {
         id: 2,
-        image: "assets/images/forcast.bmp",
+        image: "../assets/images/forcast.bmp",
         alt: "Forcast_API",
-        title:"Forcast_By_API",
+        title: "Forcast_By_API",
         summary: " A website that displays weather information of a searched location using OpenWeather's API. Previous searches are stored in local storage.",
         description: "OpenWeather.org - API",
         github: "https://github.com/AZGchip/Forcast_By_Api",
@@ -29,9 +29,9 @@ const listData = [
     },
     {
         id: 3,
-        image: "assets/images/password.bmp",
+        image: "../assets/images/password.bmp",
         alt: "password photo",
-        title:"Password_Generator",
+        title: "Password_Generator",
         summary: "A website that creates a random password according to criteria provided by the user.",
         description: "Contains a function to re-generate a password automatically if password does not contain at least one of each selected character type.",
         github: "https://github.com/AZGchip/String_Password_Generator",
@@ -39,9 +39,9 @@ const listData = [
     },
     {
         id: 4,
-        image: "assets/images/schedule.bmp",
+        image: "../assets/images/schedule.bmp",
         alt: "schedule photo",
-        title:"Schedule_Maker",
+        title: "Schedule_Maker",
         summary: "A day planner website that allows for user input to be saved to local storage under a particular time.",
         description: " Each text box represents a time and will change from green to blue to red as the day progresses. A segmented bar in each time box will display how far along in the hour you are in 15 min segments.",
         github: "https://github.com/AZGchip/schedule_maker",
@@ -49,9 +49,9 @@ const listData = [
     },
     {
         id: 5,
-        image: "assets/images/burger.bmp",
+        image: "../assets/images/burger.bmp",
         alt: "burger photo",
-        title:"Eat_Burger",
+        title: "Eat_Burger",
         summary: `A website that allows burger entries to be created, "eaten", and/or deleted, using a MySQL database to store information.`,
         description: ` A new "burger" can be added by anyone using the site, by filling in the input box and hitting "add burger". This will create a new data row in MySQL by utilizing an ORM , modles, and a controller to interact with the MySQL database. The server is currently deployed on heroku.`,
         github: "https://github.com/AZGchip/Eat_Burger",
@@ -59,7 +59,19 @@ const listData = [
     },
 ]
 function List() {
-
+    let listCards = listData.map(list => (
+        <div>
+            <img src={list.image} alt={list.alt}></img>
+            <h1>{list.title}</h1>
+            <p>{list.summary}</p>
+            <p>{list.description}</p>
+            <button href={list.github}>Github</button>
+            <button href={list.deployed}>deployed</button>
+        </div>
+    ))
+    return <div className="container">
+        {listCards}
+    </div>
 }
 
 export default List
