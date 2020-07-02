cec1ef334d664625703de4c76e8db2f9fa348a68
AZGchip <60306563+AZGchip@users.noreply.github.com>
2020-07-02 16:59:01 -0400

    Updates

---
+++ precache-manifest.251d61b876b83d3a50a1c92c5c26efab.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70b514c0e889192027e71925e33e73d2",
+    "url": "/react_portfolio/index.html"
+  },
+  {
+    "revision": "c564e5abebcd680f9d3c",
+    "url": "/react_portfolio/static/css/main.7f721753.chunk.css"
+  },
+  {
+    "revision": "99c64ea242d15b7087f8",
+    "url": "/react_portfolio/static/js/2.cd42930a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react_portfolio/static/js/2.cd42930a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c564e5abebcd680f9d3c",
+    "url": "/react_portfolio/static/js/main.bc84623f.chunk.js"
+  },
+  {
+    "revision": "5397e927921dde71372c",
+    "url": "/react_portfolio/static/js/runtime-main.d05fa38a.js"
+  },
+  {
+    "revision": "06cca9a63a75ac5f02a86036339c521b",
+    "url": "/react_portfolio/static/media/303.06cca9a6.Jpg"
+  }
+]);
